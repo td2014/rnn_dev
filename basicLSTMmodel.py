909ef7e7e524b69a8faefcddcf56722e3a3a036b
td2014 <anthonyd2004@gmail.com>
2017-07-09 13:53:52 -0700

    Added LSTM code.

--- basicLSTMmodel.py
+++ basicLSTMmodel.py
@@ -32,12 +32,18 @@ from keras.models import Model
 # Create input sequences
 #
 X_train = []
-
-upsweep=np.array((1, 2, 3, 4, 5, 6, 7))-5
-upsweep=upsweep/10.0
+up_start = 0
+up_end = 1000
+down_start=2000
+down_end=1000
+up_array = np.linspace(up_start,up_end,int(up_end-up_start))
+upsweep=np.array(up_array)-(up_end+up_start)/2.0
+upsweep=upsweep/(1.0*np.max((up_start,up_end)))
 upsweep = np.expand_dims(upsweep,axis=1)
-downsweep=np.array((10, 9, 8, 7, 6, 5, 4))-5
-downsweep=downsweep/10.0
+
+down_array = np.linspace(down_start,down_end,int(down_start-down_end))
+downsweep=np.array(down_array)-(down_start+down_end)/2.0
+downsweep=downsweep/(1.0*np.max((down_start,down_end)))
 downsweep = np.expand_dims(downsweep,axis=1)
 
 X_train.append(upsweep)
@@ -55,8 +61,8 @@ X_train = np.array(X_train)
 
 # preparing y_train
 y_train = []
-y_train.append([8])
-y_train.append([3])
+y_train.append(2*upsweep[999]-upsweep[998])
+y_train.append(2*downsweep[999]-downsweep[998])
 ###y_train.append([3])
 #y_train.append([0,1])
 #y_train.append([1,0])
@@ -72,20 +78,20 @@ y_train = np.array(y_train)
 # Create model
 #
 
-inputs = Input(shape=(7,1), name='Input1')
-###x = LSTM(units=1, name='LSTM1')(inputs)
-x = SimpleRNN(2, name='SimpleRNN1')(inputs)
+inputs = Input(shape=(1000,1), name='Input1')
+x = LSTM(units=2, name='LSTM1')(inputs)
+###x = SimpleRNN(units=2, name='SimpleRNN1')(inputs)
 x = Dense(2)(x)
 predictions = Dense(1, name='Dense1')(x)
 model = Model(inputs=inputs, outputs=predictions)
-model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='mae', optimizer='rmsprop', metrics=['accuracy'])
 print(model.summary())
 
 #
 # Train
 # 
 print('Training model...')
-model.fit(X_train, y_train, epochs=1000, batch_size=2)
+model.fit(X_train, y_train, epochs=100, batch_size=2)
 
 #
 # output predictions
